429afd7000312e05eed25fbea423855a9338ed20
lukethun <lukethun@optusnet.com.au>
2023-11-03 13:09:38 +1000

    Add index.html and javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
+
+  
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes();
+    else no();
+}
