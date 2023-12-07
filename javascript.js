04e4735de4251a7e7c95a92396b676ce60ffd9d4
lukethun <lukethun@optusnet.com.au>
2023-12-07 14:27:30 +1000

    Clean up code and make functioning score counter and game stop

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let computerSelection = "";
 let computerScore = 0;
 let playerScore = 0;
 
-console.log(Math.floor(Math.random() * 3));
+
 // Function to get random selection from computer 
 
 function getComputerChoice(){
@@ -22,16 +22,16 @@ function playGame(playerSelection, computerSelection){
   || playerSelection === "paper" && computerSelection === "scissors" 
   || playerSelection === "scissors" && computerSelection === "rock"){
     computerScore += 1;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    messageDiv.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
   } else if(playerSelection === computerSelection){
-    return "It's a draw";
+    messageDiv.textContent = "It's a draw";
   } else if (playerSelection === "paper" && computerSelection === "rock" 
   || playerSelection === "scissors" && computerSelection === "paper" 
   || playerSelection === "rock" && computerSelection === "scissors"){
     playerScore += 1;
-    return `You win! ${playerSelection} beats ${computerSelection}`
+    messageDiv.textContent = `You win! ${playerSelection} beats ${computerSelection}`
   } else {
-    return "Invalid Choice";
+    messageDiv.textContent = "Invalid Choice";
   }
 }
 
@@ -39,46 +39,52 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const scoreDiv = document.createElement('div');
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const messageDiv = document.createElement('div');
+const buttons = document.querySelectorAll('button');
 
-body.appendChild(scoreDiv);
-
-
-
+messageDiv.textContent = ' ';
+scoreDiv.textContent = '0 : 0';
+container.prepend(scoreDiv, messageDiv);
+scoreDiv.setAttribute('class', 'score');
+messageDiv.setAttribute('class', 'message');
 
 rock.addEventListener('click', () => {
   getComputerChoice();
-  console.log(playGame("rock", computerSelection));
+  playGame("rock", computerSelection);
   scoreDiv.textContent = `${playerScore} : ${computerScore}`;
+  winner();
 })
 
 paper.addEventListener('click', () => {
   getComputerChoice();
-  console.log(playGame("paper", computerSelection));
+  playGame("paper", computerSelection);
   scoreDiv.textContent = `${playerScore} : ${computerScore}`;
+  winner();
 })
 
 scissors.addEventListener('click', () => {
   getComputerChoice();
-  console.log(playGame("scissors", computerSelection));
+  playGame("scissors", computerSelection);
   scoreDiv.textContent = `${playerScore} : ${computerScore}`;
+  winner();
 })
 
-
-
-
-// Loop until a score of 5 is reached for either player or computer
-// while(computerScore < 5 && playerScore < 5){
-//   game();
-//   console.log(`Computer : ${computerScore}`);
-//   console.log(`Player : ${playerScore}`);
-// }
-
-
 // Display winner
-if(playerScore == 5){
-  console.log("You win");
-} else if(computerScore == 5){
-  console.log("You lose...");
+
+function disableButtons(){
+  buttons.forEach(button => {
+    button.disabled = true;
+  })
 }
 
+
+function winner(){
+  if(playerScore === 5){
+    messageDiv.textContent = "You win!";
+    disableButtons();
+  } else if(computerScore === 5){
+    messageDiv.textContent = 'You lose...';
+    disableButtons();
+  }
+}
